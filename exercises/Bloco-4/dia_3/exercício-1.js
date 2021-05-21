cf8fb292f6cd80ca782896fcb433e8fadecd2084
Henrique Cursino <henriquecursino46@gmail.com>
2021-05-21 18:26:35 -0300

    exercicios bloco-4/dia-3

---
+++ exercises/Bloco-4/dia_3/exercício-1.js
@@ -0,0 +1,11 @@
+let n = 10;
+let element = '*';
+let inputLine = '';
+
+for (let index = 0; index < n; index += 1) {
+    inputLine = inputLine + element;
+}
+
+for (let index = 0; index < n; index += 1) {
+    console.log(inputLine);
+}
