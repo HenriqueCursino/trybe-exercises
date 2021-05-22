69eed570471374f5d1268a164f713123bd9c346f
Henrique Cursino <henriquecursino46@gmail.com>
2021-05-22 10:35:54 -0300

    exercicio-4

--- exercises/Bloco-4/dia_3/exercício-4.js
+++ exercises/Bloco-4/dia_3/exercício-4.js
@@ -1,14 +1,17 @@
-
+// Agora n é o valor da base!
+// A altura da pirâmide está relacionado com o centro dela!
 let n = 5;
 let symbol = '*';
 let inputLine = '';
 
+// Para encontrar o centro da pirâmide é só somar 1 ao valor e dividir por 2!
 let midOfMatrix = (n + 1) / 2;
 let controlLeft = midOfMatrix;
 let controlRight = midOfMatrix;
 
 for (let lineIndex = 0; lineIndex <= midOfMatrix; lineIndex += 1) {
   for (let columnIndex = 0; columnIndex <= n; columnIndex += 1) {
+// Para verificar se está no centro!
     if (columnIndex > controlLeft && columnIndex < controlRight) {
       inputLine = inputLine + symbol;
     } else {
@@ -17,6 +20,7 @@ for (let lineIndex = 0; lineIndex <= midOfMatrix; lineIndex += 1) {
   }
   console.log(inputLine);
   inputLine = '';
+  // Partindo do meio da pirâmide!
   controlRight += 1;
   controlLeft -= 1
 }
