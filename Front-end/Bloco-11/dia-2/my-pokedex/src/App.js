1c82745a5deb088ad7983dec70d508a181edb42c
Henrique Cursino <henriquecursino46@gmail.com>
2021-07-21 19:35:59 -0300

    Pokedex atualizada

--- Front-end/Bloco-11/dia-2/my-pokedex/src/App.js
+++ Front-end/Bloco-11/dia-2/my-pokedex/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import pokemons from './data';
-//Import do json criado com os pokemons e seus dados.
+//Import do json criado com os pokemons e seus dados..
 import Pokedex from './Pokedex';
 
 class App extends React.Component {
